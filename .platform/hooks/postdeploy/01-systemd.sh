f5b1e08bcf220467da65555423682d943fa4640d
Jake Bell <jbbell@umn.edu>
2020-08-17 12:01:02 -0500

    Adding config hook for systemd units

--- .platform/hooks/postdeploy/01-systemd.sh
+++ .platform/hooks/postdeploy/01-systemd.sh
@@ -6,11 +6,15 @@ fi
 
 for file in ./.platform/systemd/*; do
   filename=$(basename "$file")
+
   cp "$file" /etc/systemd/system
   chmod 644 "/etc/systemd/system/$filename"
+
   systemctl daemon-reload
   systemctl enable "$filename"
-  if [[ "$filename" == *.timer ]]; then
+
+  # Only start units that aren't oneshots.
+  if ! grep -q 'Type=oneshot' "$file"; then
     systemctl start "$filename"
   fi
 done
