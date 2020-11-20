bcc89ab97ea54ff13cc6623cc734ba59b25366b4
Jake Bell <sigterm01@gmail.com>
2020-11-20 16:07:28 -0600

    Using `S3Boto3Storage` by default

--- core/settings/aws.py
+++ core/settings/aws.py
@@ -3,7 +3,7 @@
 import os
 
 import dj_database_url
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 from .production import *
 
@@ -34,13 +34,13 @@ STATIC_URL = '/assets/'
 MEDIA_URL = '/uploads/'
 
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     querystring_auth = False
     bucket_name = '%s-assets' % os.environ.get('S3_BUCKET_NAME')
     preload_metadata = True
 
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     bucket_name = '%s-media' % os.environ.get('S3_BUCKET_NAME')
     default_acl = 'private'
 
